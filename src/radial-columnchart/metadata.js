d8cd6d9dd7202335b3dbbab48b108ccf059a3923
Allard Warrink <allardwarrink@gmail.com>
2024-03-19 16:29:29 +0100

    added thumnail and icon

--- src/radial-columnchart/metadata.js
+++ src/radial-columnchart/metadata.js
@@ -1,10 +1,13 @@
+import icon from './radial-columnchart.svg'
+import thumbnail from './radial-columnchart_thumb.svg'
+
 export const metadata = {
-  name: 'radial-columnchart',
+  name: 'Radial column chart',
   id: 'rawgraphs.radial-columnchart',
-  // thumbnail,
-  // icon,
+  thumbnail,
+  icon,
   categories: ['correlations', 'proportions'],
-  description: 'A barchart in a circle...',
-  // code: 'https://github.com/rawgraphs',
+  description: 'It displays multiple quantitative dimensions related to categories in a circle. Variant of a stacked bar chart',
+  code: 'https://github.com/allardw/rawgraphs-radial-columnchart',
   // tutorial: 'https://rawgraphs.io/learning/',
 }
